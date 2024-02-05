d2db08921c242ef148e40c826b1a0acd6e5a8109
mika <mika.griebsch@gmail.com>
2024-02-05 14:51:41 +0100

    index aänderungen

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Griebsch Fläming Wildhandel - Produkte aus Qualitätswild';
+export const SITE_DESCRIPTION = 'Willkommen auf der Fläming Wildhandel Website';
 export const favIcon = "../public/favicon.jpg";
